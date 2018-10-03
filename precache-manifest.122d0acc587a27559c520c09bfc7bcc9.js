ef88f49fd4c756bb6ccb8c927e4069595c302e98
Dave Foderick <dfoderick@gmail.com>
2018-10-02 23:22:39 -0700

    Updates

---
+++ precache-manifest.122d0acc587a27559c520c09bfc7bcc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "afe56a1f6c29013262100cdfa9a8a91f",
+    "url": "/money-button-donate/static/media/logo.afe56a1f.svg"
+  },
+  {
+    "revision": "793d7cd6a6fe5abf59f4",
+    "url": "/money-button-donate/static/js/runtime~main.793d7cd6.js"
+  },
+  {
+    "revision": "a4d330ebe989de21bd59",
+    "url": "/money-button-donate/static/js/main.a4d330eb.chunk.js"
+  },
+  {
+    "revision": "f838b4bec7c195279970",
+    "url": "/money-button-donate/static/js/1.f838b4be.chunk.js"
+  },
+  {
+    "revision": "a4d330ebe989de21bd59",
+    "url": "/money-button-donate/static/css/main.57791097.chunk.css"
+  },
+  {
+    "revision": "f838b4bec7c195279970",
+    "url": "/money-button-donate/static/css/1.a0908d13.chunk.css"
+  },
+  {
+    "revision": "dbc269a7b80253303e0d13cb554d1319",
+    "url": "/money-button-donate/index.html"
+  }
+];
